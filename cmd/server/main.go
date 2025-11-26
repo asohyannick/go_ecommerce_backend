b9a85b5927bbe0a2d7711de1d88c4898740d61ab
asohyannick <asoh@cskytelecoms.com>
2025-11-26 13:21:10 +0100

    I've completed the project structure of my Go Restful API project with echo building the backend of an ecommerce project

---
+++ cmd/server/main.go
@@ -0,0 +1,13 @@
+package main
+import (
+	"net/http"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"message": "Hello Yannick!"})
+	})
+	e.Logger.Fatal(e.Start(":8080"))
+}
